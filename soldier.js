eff339026ab46420b28268791db5716af5146b74
khald <abv.khallid@mail.ru>
2022-05-18 10:39:16 +0300

    Created a soldier

---
+++ soldier.js
@@ -0,0 +1,20 @@
+const soldier = {
+  soldName: 'Stive',
+  soldHealth: 10,
+  soldGun: {
+    gunName: 'ak-47',
+    gunPatron: 30,
+  },
+  soldSupply: 3,
+
+  soldFire: function () {
+
+  },
+  soldRecharge: function () {
+
+  },
+  soldWound: function () {
+
+  },
+
+}
