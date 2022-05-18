3c9b610520f483ce722519737ccf045743fc9d0e
khald <abv.khallid@mail.ru>
2022-05-18 10:56:27 +0300

    Add soldier metods

--- soldier.js
+++ soldier.js
@@ -3,18 +3,45 @@ const soldier = {
   soldHealth: 10,
   soldGun: {
     gunName: 'ak-47',
-    gunPatron: 30,
+    gunPatron: 3,
   },
   soldSupply: 3,
 
   soldFire: function () {
+    if (this.soldGun.gunPatron <= 0) {
+      console.log('Обойма пуста. Перезаредитесь!');
+    } else {
+      this.soldGun.gunPatron--;
+      console.log('бах-бах');
+    }
 
   },
   soldRecharge: function () {
-
+    if (this.soldSupply <= 0) {
+      console.log('не осталось припасов :(');
+    } else {
+      this.soldSupply--;
+      this.soldGun.gunPatron = 30;
+      console.log('Перезарядка...');
+    }
   },
   soldWound: function () {
-
+    if (this.soldHealth <= 0) {
+      console.log('Ты проиграл!');
+    } else {
+      this.soldHealth--;
+    }
   },
 
 }
+
+console.log(soldier);
+soldier.soldFire();
+soldier.soldFire();
+soldier.soldFire();
+soldier.soldFire();
+
+soldier.soldRecharge();
+soldier.soldWound();
+
+console.log(soldier);
